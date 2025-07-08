d9697f542f3c317f14d14e5b2c433042861c86a0
duy1707gg <duyhl2005@gmail.com>
2025-07-08 15:28:58 +0700

    Init .NET MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace student_management_new.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
